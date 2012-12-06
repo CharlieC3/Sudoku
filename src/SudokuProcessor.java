a143ecd9be7905ecd99a4647db5b15c24c93c674
reishka <rg.NCC1701D@gmail.com>
2012-12-06 10:36:12 -0500

    Added code for sectors

--- src/SudokuProcessor.java
+++ src/SudokuProcessor.java
@@ -15,6 +15,7 @@ public class SudokuProcessor
 		{
 			first = new SudokuVertexNode(v, x, y);
 			vertices++;
+			initilizeSectors();
 			return first;
 		}
 		else 
@@ -22,6 +23,19 @@ public class SudokuProcessor
 			vertices++;
 			return new SudokuVertexNode(v, x, y);
 		}
+	}
+	
+	public void initilizeSectors()
+	{
+		sector1 = new ArrayList<SudokuVertexNode>();
+		sector2 = new ArrayList<SudokuVertexNode>();
+		sector3 = new ArrayList<SudokuVertexNode>();
+		sector4 = new ArrayList<SudokuVertexNode>();
+		sector5 = new ArrayList<SudokuVertexNode>();
+		sector6 = new ArrayList<SudokuVertexNode>();
+		sector7 = new ArrayList<SudokuVertexNode>();
+		sector8 = new ArrayList<SudokuVertexNode>();
+		sector9 = new ArrayList<SudokuVertexNode>();
 	}
 	
 	public void addNodeToSector(SudokuVertexNode n)
@@ -35,24 +49,24 @@ public class SudokuProcessor
 			switch(y)
 			{
 			case 0: case 1: case 2: sector1.add(n); break;
-			case 3: case 4: case 5: sector1.add(n); break;
-			case 6: case 7: case 8: sector1.add(n); break;
+			case 3: case 4: case 5: sector4.add(n); break;
+			case 6: case 7: case 8: sector7.add(n); break;
 			}
 			
 		case 3: case 4: case 5:
 			switch(y)
 			{
-			case 0: case 1: case 2: sector1.add(n); break;
-			case 3: case 4: case 5: sector1.add(n); break;
-			case 6: case 7: case 8: sector1.add(n); break;
+			case 0: case 1: case 2: sector2.add(n); break;
+			case 3: case 4: case 5: sector5.add(n); break;
+			case 6: case 7: case 8: sector8.add(n); break;
 			}
 			
 		case 6: case 7: case 8: 
 			switch(y)
 			{
-			case 0: case 1: case 2: sector1.add(n); break;
-			case 3: case 4: case 5: sector1.add(n); break;
-			case 6: case 7: case 8: sector1.add(n); break;
+			case 0: case 1: case 2: sector3.add(n); break;
+			case 3: case 4: case 5: sector6.add(n); break;
+			case 6: case 7: case 8: sector9.add(n); break;
 			}
 		}
 	}
@@ -88,4 +102,3 @@ public class SudokuProcessor
 
 
 }
-
